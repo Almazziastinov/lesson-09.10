a5f9028e0865f9fc07745397d47dbcc485349517
Алмаз <@LAPTOP-8MGE9JLU>
2021-10-16 08:12:23 +0300

    Добавьте файлы проекта.

---
+++ lesson 09.10/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lesson_09._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1");
+            Random rnd = new Random();
+            int[] bavar = new int[17];
+            int[] shols = new int[21];
+            Console.WriteLine(Bear(bavar, shols));
+
+
+
+            Console.WriteLine("Задание 3");
+            var students = new List<string>()
+            {
+                { "Арещенко Мария  Балл: 278\nПредметы: Рус.Яз., Математика, Физика"},
+                { "Шипило Анна  Балл: 270\nПредметы: Рус.Яз., Математика, Физика"},
+                { "Игуменов Илья  Балл 263\nПредметы: Рус.Яз., Математика, Физика" },
+                { "Зиастинов Алмаз  Балл 258\nПредметы: Рус.Яз., Математика, Физика" }
+
+            };
+            List<string> value = students;
+            foreach(var items in value)
+            {
+                Console.WriteLine(items);
+            }
+            Console.WriteLine("menu\nЕсли вы новый студент и вас нет в списке напешите: новый студен\nЕсли вы хотите удалить себя из списка впишите: удалить\nЕсли вы хотите отсортировать список впишите: сортировка ");
+            string newstud = Console.ReadLine();
+            if (newstud == "новый студен")
+            {
+                Console.WriteLine("Впишите, пожалуйста, ваши: имя, фамилия, общее количество баллов, предметы, которые сдавали");
+                string name = Console.ReadLine();
+                int ball = Convert.ToInt32(Console.ReadLine());
+                string predmety = Console.ReadLine();
+                students.Add(name);
+                students.Add(ball.ToString());
+                students.Add(predmety);
+                Console.WriteLine(items);
+            }
+            string name1 = Console.ReadLine();
+            bool conteinsValue = students.ContainsValue(name1);
+
+        }
+            static string Bear(int[]a, int[]b)
+        {
+            Random rnd = new Random();
+            int sumbv = 0;
+            int sumsh = 0;
+            for(int i = 0; i<a.Length; i++)
+            {
+                a[i] = rnd.Next(0, 9);
+                Console.Write(a[i]);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < b.Length; i++)
+            {
+                b[i] = rnd.Next(0, 9);
+                Console.Write(b[i]);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < a.Length; i++)
+            {
+                if(a[i]==5)
+                {
+                    sumbv += 1;
+                }
+
+            }
+            for (int i = 0; i < b.Length; i++)
+            {
+                if(b[i] ==5)
+                {
+                    sumsh += 1;
+                }
+
+            }
+            if(sumbv == sumsh)
+            {
+                return "все пьют";
+
+            }
+            else
+            {
+                return"никто не пьёт";
+            }
+        }
+    }
+}
---
+++ metodichka/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodichka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] vowels = "АЕЁИОУЫЭЮЯ".ToLower().ToCharArray(); //Гласные
+            char[] consonants = "БВГДЖЗЙКЛМНПРСТФХЦЧШЩЬЪ".ToLower().ToCharArray(); //Согласные
+
+            string str = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ";
+            char[] array = str.ToLower().ToCharArray();
+
+            int vowelsCount = GetCount(array, vowels);
+            int consonantsCount = GetCount(array, consonants);
+
+            Console.WriteLine("Гласных в массиве: " + vowelsCount);
+            Console.WriteLine("Согласных в массиве: " + consonantsCount);
+            Console.ReadKey();
+
+
+
+
+            Console.WriteLine("Задпние 6.2");
+            Console.WriteLine("Программа для умножения матриц");
+
+            int[,] a = GetMatrixFromConsole("A");
+            int[,] b = GetMatrixFromConsole("B");
+
+            Console.WriteLine("Матрица A:");
+            PrintMatrix(a);
+
+            Console.WriteLine("Матрица B:");
+            PrintMatrix(b);
+
+            var result = MatrixMultiplication(a, b);
+            Console.WriteLine("Произведение матриц:");
+            PrintMatrix(result);
+
+            Console.ReadLine();
+
+
+            Console.WriteLine("Задание 6.3");
+            int[,] vallue = TemprichValMatrix("A");
+            Console.WriteLine("Год матрица ");
+            PrintMatrix(vallue);
+            int[] avtemp = AverageTemprichMass(vallue);
+            for (int i = 0; i < avtemp.Length; i++)
+            {
+                Console.WriteLine(avtemp[i]);
+            }
+            Console.ReadKey();
+
+            Console.WriteLine("DZ 61");
+            var bukvyglasn = new List<string>()
+            {
+                "уеыаоэяи"
+            };
+
+        }
+
+        static Random Rand(Random a)
+        {
+            a = new Random();
+            return a;
+        }
+        static int[,] TemprichValMatrix(string a)
+        {
+            Random rnd = new Random();
+            int[,] mounth = new int[3, 3];
+            for (int i = 0; i < mounth.GetLength(0); i++)
+            {
+                for (int j = 0; j < mounth.GetLength(1); j++)
+                {
+                    mounth[i,j] = rnd.Next(-30, 30);
+                }
+            }
+
+            return mounth;
+        }
+        static int[] AverageTemprichMass(int[,] matrix)
+        {
+            int[] month = new int[matrix.GetLength(0)];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    month[i] += matrix[i, j];
+                }
+            }
+            return month;
+        }
+
+        static int[,] GetMatrixFromConsole(string name)
+        {
+            Console.Write("Количество строк матрицы {0}:    ", name);
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Количество столбцов матрицы {0}: ", name);
+            int m = int.Parse(Console.ReadLine());
+            Random rnd = new Random();
+            var matrix = new int[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write("{0}[{1},{2}] = ", name, i, j);
+                    matrix[i, j] = rnd.Next(50);
+                    Console.Write(matrix[i, j] + "\n");
+                
+                }
+            }
+
+            return matrix;
+        }
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j].ToString() + "  ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+        static int[,] MatrixMultiplication(int[,] matrixA, int[,] matrixB)
+        {
+            if (matrixA.GetLength(0) != matrixB.GetLength(1))
+            {
+                throw new Exception("Умножение не возможно! Количество столбцов первой матрицы не равно количеству строк второй матрицы.");
+            }
+
+            int[,] matrixC = new int[matrixA.GetLength(1), matrixB.GetLength(0)];
+
+            for (int i = 0; i < matrixA.GetLength(1); i++)
+            {
+                for (int j = 0; j < matrixB.GetLength(0); j++)
+                {
+                    matrixC[i, j] = 0;
+
+                    for (int k = 0; k < matrixA.GetLength(0); k++)
+                    {
+                        matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+                    }
+                }
+            }
+
+            return matrixC;
+        }
+
+        static int GetCount(char[] array, char[] ar)
+        {
+            int Count = 0;
+
+            foreach (char ch in array)
+                foreach (char cha in ar)
+                    if (ch == cha)
+                        Count++;
+
+            return Count;
+
+        }
+        
+    }
+    
+
+}
